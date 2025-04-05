c9943eb1d9d78cfb4b0a13b832516049a4e6088b
GabrielMacocco <gabrielmacocco99@gmail.com>
2025-04-05 14:24:40 -0300

    Agregamos el frontend, con HTML,CSS y todas las dependencias necesarias

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Para React o archivos JavaScript/HTML dentro de 'src'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
