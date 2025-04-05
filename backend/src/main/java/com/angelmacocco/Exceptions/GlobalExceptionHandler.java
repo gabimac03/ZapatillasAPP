46fcda563ebe03eee96aa5519bfd4472759f1e15
GabrielMacocco <gabrielmacocco99@gmail.com>
2025-04-05 14:37:59 -0300

    Agregamos las excepciones globales dentro del proyecto

---
+++ backend/src/main/java/com/angelmacocco/Exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+
+package com.angelmacocco.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> manejarExcepcionGeneral(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
