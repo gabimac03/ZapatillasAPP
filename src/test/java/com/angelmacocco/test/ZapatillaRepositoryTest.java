daf3d948a055ec470e975398d1ddeee544af3613
GabrielMacocco <gabrielmacocco99@gmail.com>
2025-04-05 13:00:24 -0300

    Ademas agregamos el metodo beforeach, para actualizar sin nada la base de datos cada vez que hacemos un test y actualizamos la zapatilla

--- src/test/java/com/angelmacocco/test/ZapatillaRepositoryTest.java
+++ src/test/java/com/angelmacocco/test/ZapatillaRepositoryTest.java
@@ -6,6 +6,7 @@ import com.angelmacocco.repository.ZapatillaRepository;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -20,7 +21,13 @@ public class ZapatillaRepositoryTest {
     @Autowired
     private ZapatillaRepository zapatillaRepository;
 
-  /*  @Test
+        
+    @BeforeEach
+    void limpiarDB() {
+            zapatillaRepository.deleteAll();
+    }
+    
+    @Test
     @Order(1)
     public void testGuardarZapatilla() {
         Zapatilla z = new Zapatilla();
@@ -33,7 +40,7 @@ public class ZapatillaRepositoryTest {
 
         Assertions.assertNotNull(guardada.getId());
     }
-/*
+    
     @Test
     @Order(2)
     public void testBuscarTodas() {
@@ -45,11 +52,10 @@ public class ZapatillaRepositoryTest {
     @Order(3)
     public void borrarZapatilla(){
         zapatillaRepository.deleteById(1L);
-    }*/
+    }
     
-   /* @Test
+    @Test
     @Order(4)
-    
     public void buscarZapatillaPorId() {
     Zapatilla z = new Zapatilla();
     z.setMarca("Nike");
@@ -63,14 +69,7 @@ public class ZapatillaRepositoryTest {
 
     Assertions.assertTrue(encontrada.isPresent());
     Assertions.assertEquals("Air Max", encontrada.get().getNombre());
-}*/
-    
-    @Test
-    @Order(2)
-        public void testBuscarTodas() {
-            List<Zapatilla> lista = zapatillaRepository.findAll();
-            Assertions.assertFalse(lista.isEmpty(), "La lista de zapatillas no debería estar vacía");
-        }
+}
 
     @Test
     @Order(5)
@@ -79,9 +78,32 @@ public class ZapatillaRepositoryTest {
         Assertions.assertFalse(zapatillas.isEmpty(), "No se encontraron zapatillas con el nombre Air Max");
     }
         
+    @Test
+    @Order(6)
+    public void testActualizarZapatilla() {
+
+        
+        Zapatilla z = new Zapatilla();
+        z.setNombre("Air Max");
+        z.setMarca("Nike");
+        z.setDescripcion("Zapatilla deportiva");
+        z.setPrecio(15000.0);
+        z.setStock(10);
+        z.setTalle(42);
+        z.setCategoria("Running");
+        z.setImagenUrl("url_imagen");
+
+        Zapatilla guardada = zapatillaRepository.save(z);
+
+        guardada.setPrecio(18000.0);
+        zapatillaRepository.save(guardada);
+
+        Optional<Zapatilla> actualizada = zapatillaRepository.findById(guardada.getId());
+        Assertions.assertTrue(actualizada.isPresent());
+        Assertions.assertEquals(18000.0, actualizada.get().getPrecio());
+}
         
         
-        
-        
+   
         
 }
